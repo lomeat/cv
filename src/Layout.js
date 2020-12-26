ee6b594eb781d7e5a9c208c90de7712200ec636e
lomeat <lom3at@gmail.com>
2020-12-26 17:06:43 +0300

    add card

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,10 @@
-import React from "react";
 import styled from "styled-components";
 
-export const Layout = ({ direction, children }) => (
-  <StLayout direction={direction}>{children}</StLayout>
-);
-
-const StLayout = styled.div`
+export const Layout = styled.div`
   display: flex;
   flex-direction: ${(props) => (props.direction ? props.direction : "row")};
+
+  @media screen and (max-width: 700px) {
+    flex-direction: column !important;
+  }
 `;
